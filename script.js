7f81bc7e9d6a15176187a84c0e0eea20ae07ded9
axellozano2028 <138075037+axellozano2028@users.noreply.github.com>
2023-06-29 09:35:02 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+
+var bottom = document.getElementById("button");
+var body = document.body;
+
+body.setAttribute("style", "background-color:gray");
+
+
+
+
+bottom.addEventListener("click", function(){
+console.log("click1");
+
+body.setAttribute("style", "background-color: dark blue; color: white;");
+
+
+
+
+});
+       
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
